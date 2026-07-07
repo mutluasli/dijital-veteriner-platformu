a605198693f685553afd9f87cc6dcc791ea2bb80
mutluasli <mutluasli24@gmail.com>
2026-07-08 01:42:55 +0300

    Sahip Ekleme Arayüzü Eklendi

--- DijitalVeterinerPlatformu/mainwindow.cpp
+++ DijitalVeterinerPlatformu/mainwindow.cpp
@@ -1,14 +1,44 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
+#include "owner.h"
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    QList<owner> owners = owner::getAll();
+    for (const owner &o : owners) {
+        ui->listWidgetSahipler->addItem(o.ad + " - " + o.telefon + " - " + o.email);
+    }
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+void MainWindow::on_btnSahipEkle_clicked()
+{
+    QString ad = ui->lineEditAd->text();
+    QString telefon = ui->lineEditTelefon->text();
+    QString email = ui->lineEditEmail->text();
+
+    if (ad.isEmpty()) {
+        return;
+    }
+
+    owner::add(ad, telefon, email);
+
+    ui->lineEditAd->clear();
+    ui->lineEditTelefon->clear();
+    ui->lineEditEmail->clear();
+
+    ui->listWidgetSahipler->clear();
+    QList<owner> owners = owner::getAll();
+    for (const owner &o : owners) {
+        ui->listWidgetSahipler->addItem(o.ad + " - " + o.telefon + " - " + o.email);
+    }
+
+}
+
--- DijitalVeterinerPlatformu/mainwindow.h
+++ DijitalVeterinerPlatformu/mainwindow.h
@@ -17,6 +17,9 @@ public:
     explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow() override;
 
+private slots:
+    void on_btnSahipEkle_clicked();
+
 private:
     Ui::MainWindow *ui;
 };
